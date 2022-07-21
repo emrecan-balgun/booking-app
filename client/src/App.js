ce60b2c6e03b5cc93e4b4774e922862d40dfd9c1
Emrecan Balgün <deathlygrave@gmail.com>
2022-07-21 15:42:46 +0300

    add header section and update app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import Category from './components/Category';
 import Header from './components/Header';
+import HeaderSection from './components/HeaderSection';
 
 function App() {
   return (
     <div className="bg-blue-800">
       <Header />
       <Category />
+      <HeaderSection />
     </div>
   );
 }
---
+++ client/src/components/HeaderSection.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function HeaderSection() {
+  return (
+    <div className="container mx-auto pt-5 pb-12 text-white">
+        <span className="font-bold text-3xl tracking-tight">A lifetime of discounts? It's Genius.</span>
+        <span className="block mt-3 mb-3">Get rewarded for your travels - unlock instant savings of 10% or more with a free BE Booking account</span>
+        <button className="bg-blue-500 border rounded-sm border-transparent py-2 px-3 text-sm hover:bg-white hover:text-blue-500">Sign in/Register</button>
+    </div>
+  )
+}
+
+export default HeaderSection
