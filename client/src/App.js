facf98271da392bcb1fd6f5e914396683c837021
Emrecan Balgün <deathlygrave@gmail.com>
2022-07-21 16:12:09 +0300

    add search section and update app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,17 @@
 import Category from './components/Category';
 import Header from './components/Header';
 import HeaderSection from './components/HeaderSection';
+import SearchSection from './components/SearchSection';
 
 function App() {
   return (
-    <div className="bg-blue-800">
-      <Header />
-      <Category />
-      <HeaderSection />
+    <div className="main">
+      <div className="bg-blue-800">
+          <Header />
+          <Category />
+          <HeaderSection />
+          <SearchSection />
+        </div>
     </div>
   );
 }
