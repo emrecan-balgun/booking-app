c5fd379b12e652f35cedb9886403396f9692700b
Emrecan Balgün <deathlygrave@gmail.com>
2022-07-21 14:48:04 +0300

    add header and update app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,9 @@
+import Header from './components/Header';
+
 function App() {
   return (
-    <div className="bg-red-300">
-      Test
+    <div className="bg-blue-800">
+      <Header />
     </div>
   );
 }
---
+++ client/src/components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div className="container mx-auto p-5">
+        <div className="flex justify-between text-white">
+            <span className="text-2xl font-semibold">BE Booking</span>
+            <div className="flex gap-2">
+                <button className="text-blue-800 bg-white border px-2 rounded-sm hover:text-white hover:bg-blue-800 transition-colors">Register</button>
+                <button className="text-blue-800 bg-white border px-2 rounded-sm hover:text-white hover:bg-blue-800 transition-colors">Login</button>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Header
