fa7194e7db6cb7e5dfc3f15f01ef4bd85bbf4cb6
Emrecan Balgün <deathlygrave@gmail.com>
2022-07-27 15:42:12 +0300

    final search filter

--- client/src/pages/Hotels.js
+++ client/src/pages/Hotels.js
@@ -3,11 +3,11 @@ import SearchResult from '../components/SearchResult';
 
 function Hotels() {
   return (
-    <div className='container mx-auto flex'>
-      <div className='w-1/4 bg-red-300'>
+    <div className='container mx-auto flex pt-5 pb-5'>
+      <div className='w-1/4'>
         <SearchFilter />
       </div>
-      <div className='w-3/4 bg-blue-300'>
+      <div className='w-3/4'>
         <SearchResult />
       </div>
     </div>
