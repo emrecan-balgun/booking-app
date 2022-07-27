260a862d77119bc92b00f74b144712038080e61b
Emrecan Balgün <deathlygrave@gmail.com>
2022-07-27 15:16:00 +0300

    add search filter and search result components, refactor hotels.js

--- client/src/pages/Hotels.js
+++ client/src/pages/Hotels.js
@@ -1,8 +1,16 @@
-import React from 'react'
+import SearchFilter from '../components/SearchFilter';
+import SearchResult from '../components/SearchResult';
 
 function Hotels() {
   return (
-    <div>Hotels</div>
+    <div className='container mx-auto flex'>
+      <div className='w-1/4 bg-red-300'>
+        <SearchFilter />
+      </div>
+      <div className='w-3/4 bg-blue-300'>
+        <SearchResult />
+      </div>
+    </div>
   )
 }
 
